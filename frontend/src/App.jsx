69ce57c8f243a0651dcdefb629edb18e1c418d8e
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-08 14:16:15 +0530

    Created Search bar component and make it functional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,11 +13,13 @@ import Product from "./pages/Product";
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
+      <SearchBar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/frontend_assets/assets";
 import { NavLink, Link } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const {setShowSearch} =useContext(ShopContext)
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to={'/'}><img src={assets.logo} className="w-36" alt="logo" /></Link>
@@ -25,7 +29,7 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
-        <img
+        <img onClick={()=>setShowSearch(true)}
           src={assets.search_icon}
           className="w-5 cursor-pointer"
           alt="search-icon"
