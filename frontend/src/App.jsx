4897374b94069aa2491ee29de4b55a6008dea837
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-11 17:35:08 +0530

    Added cart functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,9 +15,12 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import SearchBar from "./components/SearchBar";
 
+import { ToastContainer, toast } from 'react-toastify';
+
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
       <Navbar />
       <SearchBar/>
       <Routes>
