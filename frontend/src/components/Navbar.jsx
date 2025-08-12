a3305a6a6e04dcbd9d09d77d7ebdce18b360cece
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-12 14:14:08 +0530

    created Login page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,11 +5,13 @@ import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const {setShowSearch,getCartCount} =useContext(ShopContext)
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <Link to={'/'}><img src={assets.logo} className="w-36" alt="logo" /></Link>
+      <Link to={"/"}>
+        <img src={assets.logo} className="w-36" alt="logo" />
+      </Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
@@ -29,17 +31,20 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
-        <img onClick={()=>setShowSearch(true)}
+        <img
+          onClick={() => setShowSearch(true)}
           src={assets.search_icon}
           className="w-5 cursor-pointer"
           alt="search-icon"
         />
         <div className="group relative">
-          <img
-            src={assets.profile_icon}
-            className="w-5 cursor-pointer"
-            alt="profile-icon"
-          />
+          <Link to="/login" >
+            <img
+              src={assets.profile_icon}
+              className="w-5 cursor-pointer"
+              alt="profile-icon"
+            />
+          </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My profile</p>
@@ -63,17 +68,44 @@ const Navbar = () => {
       </div>
       <div
         className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white
-       transition-all ${visible?`w-full`: `w-0`}`}
+       transition-all ${visible ? `w-full` : `w-0`}`}
       >
         <div className="flex flex-col text-gray-600">
-          <div onClick={()=>setVisible(false)}className="flex cursor-pointer items-center gap-4 p-3">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex cursor-pointer items-center gap-4 p-3"
+          >
             <img src={assets.dropdown_icon} alt="" className="h-4 rotate-180" />
             <p>Back</p>
           </div>
-          <NavLink onClick={()=>setVisible(false)} className="py-2 pl-6 border" to='/'>HOME</NavLink>
-          <NavLink onClick={()=>setVisible(false)} className="py-2 pl-6 border" to='/collection'>COLLECTION</NavLink>
-          <NavLink onClick={()=>setVisible(false)} className="py-2 pl-6 border" to='/about'>ABOUT</NavLink>
-          <NavLink onClick={()=>setVisible(false)} className="py-2 pl-6 border" to='/contact'>CONTACT</NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/collection"
+          >
+            COLLECTION
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/about"
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/contact"
+          >
+            CONTACT
+          </NavLink>
         </div>
       </div>
     </div>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [currentState, setCurrentState] = useState("Sign Up");
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
 
-export default Login
+  return (
+    <form onSubmit={onSubmitHandler}
+      action=""
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-grat-800"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bbg-gray-800" />
+      </div>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          required
+          placeholder="Name"
+        />
+      )}
+
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        required
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        required
+        placeholder="Password"
+      />
+      <div className="w-full flex justify-between text-sm mt-[8px]">
+        <p className="cursor-pointer">Forgot Your Password</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer"
+          >
+            Create Account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login Here
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
+    </form>
+  );
+};
+
+export default Login;
