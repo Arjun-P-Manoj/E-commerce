0aaf84a70c011ce510ba11e95897895749511f63
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-13 23:36:54 +0530

    Setup usercontroller.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,9 @@
+const loginUser = async (req, res) => {};
+
+const registerUser = async (req, res) => {
+  res.json({ msg: "API Working" });
+};
+
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
