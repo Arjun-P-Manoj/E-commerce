32c669178b36ad2f469ff8c272ce3f2e17eae326
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-17 23:40:15 +0530

    Created app layout for admin page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/SideBar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+import { ToastContainer, toast } from "react-toastify";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
+  const [token, setToken] = useState(
+    localStorage.getItem("token" ? localStorage.getItem("token") : "")
+  );
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
+
   return (
     <div className="bg-gray-50 min-h-screen">
-      <Navbar />
-      <hr />
-      <div className="flex w-full">
-        <Sidebar />
-      </div>
+      <ToastContainer />
+      {token == "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto my-8 text-gray-600 text-base ml-[max(5vw,25px)] ">
+              <Routes>
+                <Route path="/add" element={<Add />}></Route>
+                <Route path="/list" element={<List />}></Route>
+                <Route path="/orders" element={<Orders />}></Route>
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
