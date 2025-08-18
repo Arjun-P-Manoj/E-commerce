2e78f5e58939dfc2b3cd098b2da8f0be87219912
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-18 13:27:34 +0530

    successfully fetch data from backend list api

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { backendUrl } from "../App";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+  const [list, setList] = useState("");
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      if (response.data.success) {
+        setList(response.data.products);
+        console.log(list);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message);
+    }
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
+  return <div>List</div>;
+};
 
-export default List
+export default List;
