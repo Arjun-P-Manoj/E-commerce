959edb3febf48e67a9ae746cb7ebb35dca5b0985
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-24 20:04:20 +0530

    created Ordercontroller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,18 @@
+//cod payment
+const placeOrder = async () => {};
+const placeOrderStripe = async (req, res) => {};
+const placeOrderRazorPay = async (req, res) => {};
+//user order data for adminpanel
+const allOrders = async (req, res) => {};
+//user order data for frontend
+const userOrders = async (req, res) => {};
+const updateStatus = async (req, res) => {};
+
+export {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorPay,
+  allOrders,
+  userOrders,
+  updateStatus,
+};
