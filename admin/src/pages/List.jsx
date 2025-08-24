996fa49262169703fa6d1646c6fbf2cb0d85ad97
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-24 19:11:45 +0530

    updated List.jsx

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -58,7 +58,7 @@ const List = ({ token }) => {
         }
         {list.map((item, index) => (
           <div
-            className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-xs"
+            className="grid grid-cols-[1fr_3fr_1fr_1fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-xs"
             key={index}
           >
             <img className="w-5/12" src={item.image[0]} alt="" />
