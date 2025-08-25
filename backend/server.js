d9930ca117a5b77bd02c538d86258c9235fe5641
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-25 18:28:19 +0530

    Fix CORS for frontend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,14 @@ connectCloudinary();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://forever-frontend-peach-phi.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 //api endpoints
 
