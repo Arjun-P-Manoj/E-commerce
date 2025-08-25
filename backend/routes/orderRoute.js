125df9edab1d1d65b6af59156118c108e7046f1b
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-25 13:26:56 +0530

    created orderroute

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  placeOrder,
+  placeOrderStripe,
+  allOrders,
+  updateStatus,
+  userOrders,
+  placeOrderRazorPay,
+} from "../controllers/orderController.js";
+import adminAuth from "../middleware/adminauth.js";
+import authUser from "../middleware/auth.js";
+
+const orderRouter = express.Router();
+
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
+
+orderRouter.post("/place", authUser, placeOrder);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+orderRouter.post("/razorpay", authUser, placeOrderRazorPay);
+
+orderRouter.post("/userorders", authUser, userOrders);
+
+export default orderRouter;
