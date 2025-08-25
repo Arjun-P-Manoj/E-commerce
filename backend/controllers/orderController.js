3a05824316b4bf20621a997d8dd6f7bcb1a596ef
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-08-25 13:28:50 +0530

    Added logic for placeorder through cod,updatestatus,userorders and admin oreder

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,12 +1,65 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
 //cod payment
-const placeOrder = async () => {};
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+
+    res.json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 const placeOrderStripe = async (req, res) => {};
 const placeOrderRazorPay = async (req, res) => {};
 //user order data for adminpanel
-const allOrders = async (req, res) => {};
+const allOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 //user order data for frontend
-const userOrders = async (req, res) => {};
-const updateStatus = async (req, res) => {};
+const userOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const orders = await orderModel.find({ userId });
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const updateStatus = async (req, res) => {
+  try {
+    const { orderId, status } = req.body;
+    await orderModel.findByIdAndUpdate(orderId, { status });
+    res.json({ success: true, message: "Staus Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export {
   placeOrder,
